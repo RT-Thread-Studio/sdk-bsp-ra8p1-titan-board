bddf74c144785f432fcc5f10250976c3b66c2e25
yans <liuzhibin@rt-thread.com>
2025-09-03 10:39:47 +0800

    fix ci error

--- project/Titan_driver_rs485/board/lvgl/lv_port_disp.c
+++ project/Titan_driver_rs485/board/lvgl/lv_port_disp.c
@@ -8,139 +8,73 @@
  * 2021-11-24     Rbb666       The first version
  */
 #include <lvgl.h>
+#include <rtthread.h>
+#include "ra8/lcd_config.h"
 #include "hal_data.h"
 
-#if DLG_LVGL_USE_GPU_RA6M3
-    #include "drv_g2d.h"
-#endif
+static rt_sem_t _SemaphoreVsync = RT_NULL;
+static uint8_t lvgl_init_flag = 0;
 
-#ifdef PKG_USING_ILI9341
-    #include "lcd_ili9341.h"
-#else
-    #include "lcd_port.h"
-#endif
-
-#define COLOR_BUFFER  (LV_HOR_RES_MAX * LV_VER_RES_MAX / 4)
-
-/*A static or global variable to store the buffers*/
-static lv_disp_draw_buf_t disp_buf;
-
-/*Descriptor of a display driver*/
-static lv_disp_drv_t disp_drv;
-static struct rt_device_graphic_info info;
-
-/*Static or global buffer(s). The second buffer is optional*/
-// 0x1FFE0000    0x20040000
-lv_color_t buf_1[COLOR_BUFFER];
-
-#if !DLG_LVGL_USE_GPU_RA6M3
-static void color_to16_maybe(lv_color16_t *dst, lv_color_t *src)
+void DisplayVsyncCallback(display_callback_args_t *p_args)
 {
-#if (LV_COLOR_DEPTH == 16)
-    dst->full = src->full;
-#else
-    dst->ch.blue = src->ch.blue;
-    dst->ch.green = src->ch.green;
-    dst->ch.red = src->ch.red;
-#endif
-}
-#endif
-
-void _ra_port_display_callback(display_callback_args_t *p_args)
-{
-    /* TFT-Callback */
-}
-
-static void disp_flush(lv_disp_drv_t *disp_drv, const lv_area_t *area, lv_color_t *color_p)
-{
-#ifdef PKG_USING_ILI9341
-    lcd_fill_array_spi(area->x1, area->y1, area->x2, area->y2, color_p);
-#elif DLG_LVGL_USE_GPU_RA6M3
-    lv_port_gpu_blit(area->x1, area->y1, color_p, area);
-#else
-    int x1, x2, y1, y2;
-
-    x1 = area->x1;
-    x2 = area->x2;
-    y1 = area->y1;
-    y2 = area->y2;
-
-    /*Return if the area is out the screen*/
-    if (x2 < 0)
-        return;
-    if (y2 < 0)
-        return;
-    if (x1 > info.width - 1)
-        return;
-    if (y1 > info.height - 1)
-        return;
-
-    /*Truncate the area to the screen*/
-    int32_t act_x1 = x1 < 0 ? 0 : x1;
-    int32_t act_y1 = y1 < 0 ? 0 : y1;
-    int32_t act_x2 = x2 > info.width - 1 ? info.width - 1 : x2;
-    int32_t act_y2 = y2 > info.height - 1 ? info.height - 1 : y2;
-
-    uint32_t x;
-    uint32_t y;
-    long int location = 0;
-
-    /* color_p is a buffer pointer; the buffer is provided by LVGL */
-    lv_color16_t *fbp16 = (lv_color16_t *)info.framebuffer;
-
-    for (y = act_y1; y <= act_y2; y++)
+    rt_interrupt_enter();
+    if (DISPLAY_EVENT_LINE_DETECTION == p_args->event)
     {
-        for (x = act_x1; x <= act_x2; x++)
-        {
-            location = (x) + (y) * info.width;
-            color_to16_maybe(&fbp16[location], color_p);
-            color_p++;
-        }
-
-        color_p += x2 - act_x2;
+        if (lvgl_init_flag != 0)
+            rt_sem_release(_SemaphoreVsync);
     }
+    rt_interrupt_leave();
+}
+
+static void vsync_wait_cb(lv_display_t *display)
+{
+    if (!lv_display_flush_is_last(display)) return;
+
+    //
+    // If Vsync semaphore has already been set, clear it then wait to avoid tearing
+    //
+    rt_sem_take(_SemaphoreVsync, RT_WAITING_FOREVER);
+}
+
+static void disp_flush(lv_display_t *display, const lv_area_t *area, uint8_t *px_map)
+{
+    if (!lv_display_flush_is_last(display)) return;
+
+#if (BSP_CFG_DCACHE_ENABLED)
+    int32_t size;
+    /* Invalidate cache - so the HW can access any data written by the CPU */
+    size = sizeof(fb_background[0]);
+
+    SCB_CleanInvalidateDCache_by_Addr(px_map, size);
 #endif
-    lv_disp_flush_ready(disp_drv);
+
+    R_GLCDC_BufferChange(&g_display0_ctrl,
+                         (uint8_t *) px_map,
+                         (display_frame_layer_t) 0);
 }
 
 void lv_port_disp_init(void)
 {
-#ifdef PKG_USING_ILI9341
-    spi_lcd_init(20);
-#else
     static rt_device_t device;
     /* LCD Device Init */
     device = rt_device_find("lcd");
     RT_ASSERT(device != RT_NULL);
 
-    if (rt_device_open(device, RT_DEVICE_OFLAG_RDWR) == RT_EOK)
+    _SemaphoreVsync = rt_sem_create("lvgl_sem", 1, RT_IPC_FLAG_PRIO);
+
+    if (RT_NULL == _SemaphoreVsync)
     {
-        rt_device_control(device, RTGRAPHIC_CTRL_GET_INFO, &info);
+        rt_kprintf("lvgl semaphore create failed\r\n");
+        RT_ASSERT(0);
     }
 
-    RT_ASSERT(info.bits_per_pixel == 8 || info.bits_per_pixel == 16 ||
-              info.bits_per_pixel == 24 || info.bits_per_pixel == 32);
-#endif
-    /*Initialize `disp_buf` with the buffer(s). With only one buffer use NULL instead buf_2 */
-    lv_disp_draw_buf_init(&disp_buf, buf_1, NULL, COLOR_BUFFER);
+    /*------------------------------------
+     * Create a display and set a flush_cb
+     * -----------------------------------*/
+    lv_display_t *disp = lv_display_create(LV_HOR_RES_MAX, LV_VER_RES_MAX);
+    lv_display_set_flush_cb(disp, disp_flush);
+    lv_display_set_flush_wait_cb(disp, vsync_wait_cb);
+    lv_display_set_buffers(disp, &fb_background[0][0], &fb_background[1][0], sizeof(fb_background[0]), LV_DISPLAY_RENDER_MODE_DIRECT);
 
-    lv_disp_drv_init(&disp_drv); /*Basic initialization*/
-
-    /*Set the resolution of the display*/
-    disp_drv.hor_res = LV_HOR_RES_MAX;
-    disp_drv.ver_res = LV_VER_RES_MAX;
-
-    /*Set a display buffer*/
-    disp_drv.draw_buf = &disp_buf;
-
-    /*Used to copy the buffer's content to the display*/
-    disp_drv.flush_cb = disp_flush;
-
-#if DLG_LVGL_USE_GPU_RA6M3
-    /* Initialize GPU module */
-    G2d_Drv_HWInit();
-#endif /* LV_PORT_DISP_GPU_EN */
-
-    /*Finally register the driver*/
-    lv_disp_drv_register(&disp_drv);
+    lvgl_init_flag = 1;
 }
--- project/Titan_driver_spi_bmi088/board/lvgl/lv_conf.h
+++ project/Titan_driver_spi_bmi088/board/lvgl/lv_conf.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2006-2023, RT-Thread Development Team
+ * Copyright (c) 2006-2024, RT-Thread Development Team
  *
  * SPDX-License-Identifier: Apache-2.0
  *
@@ -13,54 +13,69 @@
 
 #include <rtconfig.h>
 
-/* Enable additional color format support */
-#define DLG_LVGL_CF             1
+#define LV_USE_SYSMON           1
+#define LV_USE_PERF_MONITOR     1
+#define LV_COLOR_DEPTH          16
 
-/* Enable sub byte color formats to be swapped. If disabled, which is recommended for
- * performance, bitmaps need to be in correct order */
-#define DLG_LVGL_CF_SUB_BYTE_SWAP   0
+#define LV_HOR_RES_MAX          800
+#define LV_VER_RES_MAX          480
+#define LV_COLOR_16_SWAP        0
+#define LV_DPI_DEF              144
 
-#define DLG_LVGL_USE_GPU_RA6M3      0
+#ifdef BSP_USING_LVGL_DAVE2D
+    #define LV_USE_DRAW_DAVE2D      1
+#endif
 
-#define LV_USE_PERF_MONITOR         1
-#define LV_COLOR_DEPTH              16
+/*turn-on helium acceleration when Arm-2D and the Helium-powered device are detected */
+#ifdef BSP_USING_LVGL_ARM2D
+    #if defined(__ARM_FEATURE_MVE) && __ARM_FEATURE_MVE
+        #define LV_USE_DRAW_SW_ASM  LV_DRAW_SW_ASM_HELIUM
+        #define LV_USE_DRAW_ARM2D       1
+        #define LV_USE_DRAW_ARM2D_SYNC  1
+    #endif
+#endif
 
-#ifdef PKG_USING_ILI9341
-    #define LV_HOR_RES_MAX          240
-    #define LV_VER_RES_MAX          320
-    #define LV_COLOR_16_SWAP        1
-    #define LV_DPI_DEF              99
+#ifdef BSP_USING_LVGL_WIDGETS_DEMO
+    #define LV_USE_DEMO_WIDGETS 1
+    #define LV_DEMO_WIDGETS_SLIDESHOW   0
+#endif  /* BSP_USING_LVGL_WIDGETS_DEMO */
+
+/*Benchmark your system*/
+#ifdef BSP_USING_LVGL_BENCHMARK_DEMO
+    #define LV_USE_DEMO_BENCHMARK 1
+    /*Use RGB565A8 images with 16 bit color depth instead of ARGB8565*/
+    #define LV_DEMO_BENCHMARK_RGB565A8  1
+    #define LV_FONT_MONTSERRAT_14       1
+    #define LV_FONT_MONTSERRAT_20       1
+    #define LV_FONT_MONTSERRAT_24       1
+    #define LV_FONT_MONTSERRAT_26       1
+#endif  /* BSP_USING_LVGL_BENCHMARK_DEMO */
+
+/*Stress test for LVGL*/
+#ifdef BSP_USING_LVGL_STRESS_DEMO
+    #define LV_USE_DEMO_STRESS 1
+#endif  /* BSP_USING_LVGL_STRESS_DEMO */
+
+/*Render test for LVGL*/
+#ifdef BSP_USING_LVGL_RENDER_DEMO
+    #define LV_USE_DEMO_RENDER 1
+#endif  /* BSP_USING_LVGL_RENDER_DEMO */
+
+/*Music player demo*/
+#ifdef BSP_USING_LVGL_MUSIC_DEMO
+    #define LV_USE_DEMO_MUSIC 1
+    #define LV_DEMO_MUSIC_SQUARE    0
+    #define LV_DEMO_MUSIC_LANDSCAPE 0
+    #define LV_DEMO_MUSIC_ROUND     1
+    #define LV_DEMO_MUSIC_LARGE     0
+    #define LV_DEMO_MUSIC_AUTO_PLAY 1
+#ifdef LV_DEMO_MUSIC_LARGE
+    #define LV_FONT_MONTSERRAT_22   1
+    #define LV_FONT_MONTSERRAT_32   1
 #else
-    #define LV_HOR_RES_MAX          480
-    #define LV_VER_RES_MAX          272
-    #define LV_DPI_DEF              89
-#endif
-
-#ifdef BSP_USING_LVGL_VIDEO_DEMO
-#define LV_USE_FILE_EXPLORER    1
-#if LV_USE_FILE_EXPLORER
-    /*Maximum length of path*/
-    #define LV_FILE_EXPLORER_PATH_MAX_LEN        (128)
-    /*Quick access bar, 1:use, 0:not use*/
-    /*Requires: lv_list*/
-    #define LV_FILE_EXPLORER_QUICK_ACCESS        0
-#endif
-
-#define LV_USE_FS_STDIO 1
-#if LV_USE_FS_STDIO
-    #define LV_FS_STDIO_LETTER '/'      /*Set an upper cased letter on which the drive will accessible (e.g. 'A')*/
-    #define LV_FS_STDIO_PATH "/"        /*Set the working directory. File/directory paths will be appended to it.*/
-    #define LV_FS_STDIO_CACHE_SIZE  0   /*>0 to cache this number of bytes in lv_fs_read()*/
-#endif
-#endif
-
-#ifdef PKG_USING_LV_MUSIC_DEMO
-/* music player demo */
-#define LV_USE_DEMO_RTT_MUSIC       1
-#define LV_DEMO_RTT_MUSIC_AUTO_PLAY 1
-#define LV_FONT_MONTSERRAT_12       1
-#define LV_FONT_MONTSERRAT_16       1
-#define LV_COLOR_SCREEN_TRANSP      0
-#endif /* PKG_USING_LV_MUSIC_DEMO */
+    #define LV_FONT_MONTSERRAT_12   1
+    #define LV_FONT_MONTSERRAT_16   1
+#endif  /* LV_DEMO_MUSIC_LARGE */
+#endif  /* BSP_USING_LVGL_MUSIC_DEMO */
 
 #endif
